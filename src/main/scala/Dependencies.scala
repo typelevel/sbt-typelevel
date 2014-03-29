42725858d524e01132b7cc0b3f43c196f2b29880
Lars Hupel <lars.hupel@mytum.de>
2014-03-29 23:21:51 +0100

    make the checking task fail on errors

--- src/main/scala/Dependencies.scala
+++ src/main/scala/Dependencies.scala
@@ -29,25 +29,36 @@ object Dependencies {
     all.find(d => d.groupID == groupID && d.artifactID == artifactID)
   }
 
-  def check(all: List[Dependency], logger: Logger, modules: Seq[Module]) = modules.foreach {
-    case Module(ModuleId(groupID, artifactID, olderV), _, _, Some(newerV), _) =>
-      val msg = s"Version mismatch in $groupID#$artifactID: $olderV and $newerV are different"
+  def check(all: List[Dependency], logger: Logger, modules: Seq[Module]) = {
+    val problems = modules.collect {
+      case Module(ModuleId(groupID, artifactID, olderV), _, _, Some(newerV), _) =>
+        val msg = s"Version mismatch in $groupID#$artifactID: $olderV and $newerV are different"
 
-      findFromIDs(all, groupID, artifactID) match {
-        case Some(dep) =>
-          (Version.fromString(olderV), Version.fromString(newerV)) match {
-            case (Some(o), Some(n)) =>
-              if (dep.minCompatible <= o.series && o.compatible(n))
-                logger.info(s"Compatible versions in $groupID#$artifactID: $olderV and $newerV")
-              else
-                logger.error(msg)
-            case _ =>
-              logger.error(msg + ", and version numbers cannot be parsed")
-          }
-        case None =>
-          logger.error(msg + ", and no dependency information available")
-      }
-    case _ =>
+        findFromIDs(all, groupID, artifactID) match {
+          case Some(dep) =>
+            (Version.fromString(olderV), Version.fromString(newerV)) match {
+              case (Some(o), Some(n)) =>
+                if (dep.minCompatible <= o.series && o.compatible(n)) {
+                  logger.info(s"Compatible versions in $groupID#$artifactID: $olderV and $newerV")
+                  false
+                }
+                else {
+                  logger.error(msg)
+                  true
+                }
+              case _ =>
+                logger.error(msg + ", and version numbers cannot be parsed")
+                true
+            }
+          case None =>
+            logger.error(msg + ", and no dependency information available")
+            true
+        }
+    }
+
+    val count = problems.filter(identity).length
+    if (count > 0)
+      sys.error(s"Dependency check failed, found $count version mismatch(es)")
   }
 
 }
