57487bc4cfbe52c82ea7d4849c70642941cf146f
Arman Bilge <armanbilge@gmail.com>
2022-01-09 21:19:02 +0000

    Extract formatting plugin

--- core/src/main/scala/org/typelevel/sbt/TypelevelPlugin.scala
+++ core/src/main/scala/org/typelevel/sbt/TypelevelPlugin.scala
@@ -61,7 +61,7 @@ object TypelevelPlugin extends AutoPlugin {
       } yield MatrixExclude(Map("scala" -> scala, "java" -> java.render))
     },
     githubWorkflowBuild ~= { steps =>
-      WorkflowStep.Sbt(List("headerCheckAll", "scalafmtCheckAll", "scalafmtSbtCheck")) +: steps
+      WorkflowStep.Sbt(List("headerCheckAll")) +: steps
     }
   )
 
