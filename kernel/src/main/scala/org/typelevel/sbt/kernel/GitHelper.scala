d581ef24a1f4dec7bd8291587134e5b7ada4af46
Arman Bilge <armanbilge@gmail.com>
2022-01-18 10:28:28 +0000

    Make previousReleases strictly previous

--- kernel/src/main/scala/org/typelevel/sbt/kernel/GitHelper.scala
+++ kernel/src/main/scala/org/typelevel/sbt/kernel/GitHelper.scala
@@ -23,13 +23,15 @@ import scala.sys.process._
 object GitHelper {
 
   /**
+   * Returns a list of strictly previous releases (i.e. ignores tags on HEAD).
    * @param fromHead
    *   if `true`, only tags reachable from HEAD's history. If `false`, all tags in the repo.
    */
   def previousReleases(fromHead: Boolean = false): List[V] = {
     Try {
       val merged = if (fromHead) " --merged" else ""
-      s"git tag --list$merged".!!.split("\n").toList.map(_.trim).collect {
+      // --no-contains omits tags on HEAD
+      s"git tag --no-contains$merged".!!.split("\n").toList.map(_.trim).collect {
         case V.Tag(version) => version
       }
     }.getOrElse(List.empty).sorted.reverse
