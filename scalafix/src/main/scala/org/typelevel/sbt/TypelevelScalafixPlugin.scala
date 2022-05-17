b2547ba614c80784132d315056d96782ae8ed27d
David Gregory <david.gregory@opencastsoftware.com>
2022-05-17 20:25:10 +0100

    Add copyright header and remove tlScalafixDependencies key

--- scalafix/src/main/scala/org/typelevel/sbt/TypelevelScalafixPlugin.scala
+++ scalafix/src/main/scala/org/typelevel/sbt/TypelevelScalafixPlugin.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.typelevel.sbt
 
 import sbt._, Keys._
@@ -10,9 +26,7 @@ object TypelevelScalafixPlugin extends AutoPlugin {
 
   override def trigger = allRequirements
 
-  object autoImport {
-    val tlScalafixDependencies = settingKey[Seq[ModuleID]]("The scalafix rule dependencies to enable in the build.")
-  }
+  object autoImport {}
 
   import autoImport._
 
@@ -20,9 +34,8 @@ object TypelevelScalafixPlugin extends AutoPlugin {
     semanticdbEnabled := true,
     semanticdbVersion := scalafixSemanticdb.revision,
     scalafixScalaBinaryVersion := CrossVersion.binaryScalaVersion(scalaVersion.value),
-    tlScalafixDependencies := Seq(
+    scalafixDependencies ++= Seq(
       "com.github.liancheng" %% "organize-imports" % "0.6.0"
-    ),
-    scalafixDependencies ++= tlScalafixDependencies.value
+    )
   )
 }
