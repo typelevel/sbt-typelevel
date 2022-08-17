d5b95ced810ae632a66c67a1c6ca0218447d8fee
Andrew Valencik <andrew.valencik@gmail.com>
2022-08-16 21:02:04 -0400

    Tweak and add more version tests

--- kernel/src/test/scala/org/typelevel/sbt/kernel/VSuite.scala
+++ kernel/src/test/scala/org/typelevel/sbt/kernel/VSuite.scala
@@ -20,56 +20,83 @@ import munit.FunSuite
 
 class VSuite extends FunSuite {
 
-  test("current versions don't need bincompat with future versions") {
-    val currentV = V(1, 1, None, None)
-    val nextV    = V(1, 2, None, None)
-    assertEquals(currentV.mustBeBinCompatWith(nextV), false)
-  }
-
   test("1.1 needs bincompat with 1.0") {
     val currentV = V(1, 1, None, None)
-    val prevV    = V(1, 0, None, None)
+    val prevV = V(1, 0, None, None)
     assertEquals(currentV.mustBeBinCompatWith(prevV), true)
+  }
+
+  test("1.1 does not need bincompat with 1.2") {
+    val currentV = V(1, 1, None, None)
+    val nextV = V(1, 2, None, None)
+    assertEquals(currentV.mustBeBinCompatWith(nextV), false)
   }
 
   test("2.0 does not need bincompat with 1.9") {
     val currentV = V(2, 0, None, None)
-    val prevV    = V(1, 9, None, None)
+    val prevV = V(1, 9, None, None)
     assertEquals(currentV.mustBeBinCompatWith(prevV), false)
   }
 
-  test("1.1 needs bincompat with self") {
-    val currentV = V(1, 1, None, None)
-    assertEquals(currentV.mustBeBinCompatWith(currentV), true)
+  test("1.1.1 needs bincompat with 1.1.0") {
+    val currentV = V(1, 1, Some(1), None)
+    val prevV = V(1, 1, Some(0), None)
+    assertEquals(currentV.mustBeBinCompatWith(prevV), true)
   }
 
-  test("1.1 does not need bincompat with old prerelease") {
+  test("1.1 does not need bincompat with 1.0-M5") {
     val currentV = V(1, 1, None, None)
-    val prevV    = V(1, 0, None, Some("M5"))
+    val prevV = V(1, 0, None, Some("M5"))
     assertEquals(currentV.mustBeBinCompatWith(prevV), false)
   }
 
   test("0.5 does not need bincompat with 0.4") {
     val currentV = V(0, 5, None, None)
-    val prevV    = V(0, 4, None, None)
-    assertEquals(currentV.mustBeBinCompatWith(prevV), false)
-  }
-
-  test("0.5 needs bincompat with self") {
-    val currentV = V(0, 5, None, None)
-    assertEquals(currentV.mustBeBinCompatWith(currentV), true)
-  }
-
-  test("0.5.1 does not need bincompat with 0.5 ???") {
-    val currentV = V(0, 5, Some(1), None)
-    val prevV    = V(0, 5, None, None)
+    val prevV = V(0, 4, None, None)
     assertEquals(currentV.mustBeBinCompatWith(prevV), false)
   }
 
   test("0.5.1 needs bincompat with 0.5.0") {
     val currentV = V(0, 5, Some(1), None)
-    val prevV    = V(0, 5, Some(0), None)
+    val prevV = V(0, 5, Some(0), None)
     assertEquals(currentV.mustBeBinCompatWith(prevV), true)
+  }
+
+  test("0.5.1 needs bincompat with 0.5.0") {
+    val currentV = V(0, 5, Some(1), None)
+    val prevV = V(0, 5, Some(0), None)
+    assertEquals(currentV.mustBeBinCompatWith(prevV), true)
+  }
+
+  test("0.0.2 needs bincompat with 0.0.1") {
+    val currentV = V(0, 0, Some(1), None)
+    val prevV = V(0, 0, Some(0), None)
+    assertEquals(currentV.mustBeBinCompatWith(prevV), true)
+  }
+
+  test("x.y needs bincompat with self") {
+    val v0 = V(0, 5, None, None)
+    assertEquals(v0.mustBeBinCompatWith(v0), true)
+    val v1 = V(1, 5, None, None)
+    assertEquals(v1.mustBeBinCompatWith(v1), true)
+  }
+
+  test("x.y.1 does not need bincompat with x.y") {
+    val currentV = V(1, 5, Some(1), None)
+    val prevV = V(1, 5, None, None)
+    assertEquals(currentV.mustBeBinCompatWith(prevV), false)
+    val currentV0 = V(0, 5, Some(1), None)
+    val prevV0 = V(0, 5, None, None)
+    assertEquals(currentV0.mustBeBinCompatWith(prevV0), false)
+  }
+
+  test("x.y.1 < x.y") {
+    val patch0 = V(0, 5, Some(1), None)
+    val nopatch0 = V(0, 5, None, None)
+    assertEquals(patch0 < nopatch0, true)
+    val patch1 = V(1, 5, Some(1), None)
+    val nopatch1 = V(1, 5, None, None)
+    assertEquals(patch1 < nopatch1, true)
   }
 
 }
