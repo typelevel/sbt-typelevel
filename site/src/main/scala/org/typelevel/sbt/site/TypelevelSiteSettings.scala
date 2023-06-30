81c8143c733d8b872b5bd8540391ac71f66e20c5
Jamie Willis <j.willis19@imperial.ac.uk>
2023-06-30 16:05:09 +0100

    Inverted colour theme, added interim landing page gradient until the text colour is decoupled

--- site/src/main/scala/org/typelevel/sbt/site/TypelevelSiteSettings.scala
+++ site/src/main/scala/org/typelevel/sbt/site/TypelevelSiteSettings.scala
@@ -26,6 +26,7 @@ import laika.theme.config.Color
 import org.typelevel.sbt.TypelevelGitHubPlugin.gitHubUserRepo
 import sbt.Def._
 import sbt.Keys.licenses
+import laika.ast.Path.Root
 
 object TypelevelSiteSettings {
 
@@ -58,20 +59,27 @@ object TypelevelSiteSettings {
 
   // light theme colours
   // Tl suffix indicates these are lifted directly from somewhere within the Typelevel site
-  val redTl = Color.hex("f8293a")
+  //val redTl = Color.hex("f8293a")
   val brightRedTl = Color.hex("fe4559")
   val coralTl = Color.hex("f86971")
   val pinkTl = Color.hex("ffb4b5")
   val whiteTl = Color.hex("ffffff")
   val gunmetalTl = Color.hex("21303f")
   val platinumTl = Color.hex("e6e8ea") // e2e4e6?
-  val offWhiteTl = Color.hex("f2f3f4")
+  //val offWhiteTl = Color.hex("f2f3f4")
   // Extra colours to supplement
   val lightPink = Color.hex("ffe7e7")
-  val lightPinkGrey = Color.hex("f7f3f3") // f6f0f0
+  //val lightPinkGrey = Color.hex("f7f3f3")
+  //val lightPinkGreyButDarker = Color.hex("efe7e7")
   val slateBlue = Color.hex("335C67")
-  val lightSlateBlue = Color.hex("ddeaed") // cce0e4
+  val mediumSlateBlue = Color.hex("b0cfd8")
+  val lightSlateBlue = Color.hex("ddeaed")
+  val lighterSlateBlue = Color.hex("f4f8fa")
   val softYellow = Color.hex("f9f5d9") // f3eab2
+
+  val interimBlueDark = Color.hex("385a70")
+  val interimBlueMedium = Color.hex("406881")
+  val interimBlueLight = Color.hex("487692")
 
   val defaults: Initialize[Helium] = setting {
     GenericSiteSettings
@@ -95,21 +103,24 @@ object TypelevelSiteSettings {
       )
       .site
       .themeColors(
-        primary = redTl,
-        secondary = slateBlue,
-        primaryMedium = coralTl,
-        primaryLight = lightPinkGrey,
+        primary = interimBlueDark,
+        secondary = brightRedTl,
+        // the medium is on slate until the landing page is decoupled
+        primaryMedium = lightSlateBlue, // will be when the landing page text is decoupled mediumSlateBlue
+        primaryLight = lighterSlateBlue,
         text = gunmetalTl,
         background = whiteTl,
-        bgGradient = (platinumTl, offWhiteTl)
+        // interim colours, while we wait for light gradient support on landing pages
+        //bgGradient = (mediumSlateBlue, lighterSlateBlue)
+        bgGradient = (interimBlueMedium, interimBlueLight)
       )
       .site
       .messageColors(
-        info = slateBlue,
+        info = interimBlueDark,
         infoLight = lightSlateBlue,
-        warning = slateBlue,
+        warning = interimBlueDark,
         warningLight = softYellow,
-        error = slateBlue,
+        error = interimBlueDark,
         errorLight = lightPink
       )
       .site
@@ -129,6 +140,41 @@ object TypelevelSiteSettings {
           coralTl // type/class name
         )
       )
+    // just for testing purposes :) this can be removed later when things are finalised
+    /*.site.landingPage(
+    logo = Some(Image.external(s"https://typelevel.org/img/logo.svg")),
+    title = Some("SBT Typelevel"),
+    subtitle = Some("Build Configuration, Done Better than Yesterday?"),
+    latestReleases = Seq(
+      ReleaseInfo("Latest Stable Release", "0.4.23"),
+      ReleaseInfo("Latest Milestone Release", "0.5.0-M5")
+    ),
+    license = Some("Apache 2.0"),
+    titleLinks = Seq(
+      VersionMenu.create(unversionedLabel = "Getting Started"),
+      LinkGroup.create(
+        IconLink.external("https://github.com/abcdefg/", HeliumIcon.github),
+        IconLink.external("https://gitter.im/abcdefg/", HeliumIcon.chat),
+        IconLink.external("https://twitter.com/abcdefg/", HeliumIcon.twitter)
+      )
+    ),
+    documentationLinks = Seq(
+      TextLink.internal(Root / "site.md", "sbt-typelevel-site"),
+      TextLink.internal(Root / "index_.md", "home")
+    ),
+    projectLinks = Seq(
+      TextLink.internal(Root / "site.md", "Text Link"),
+      ButtonLink.external("http://somewhere.com/", "Button Label"),
+      LinkGroup.create(
+        IconLink.internal(Root / "site.md", HeliumIcon.demo),
+        IconLink.internal(Root / "site.md", HeliumIcon.info)
+      )
+    ),
+    teasers = Seq(
+      Teaser("Teaser 1", "Description 1"),
+      Teaser("Teaser 2", "Description 2"),
+      Teaser("Teaser 3", "Description 3")
+    ))*/
   }
 
 }
